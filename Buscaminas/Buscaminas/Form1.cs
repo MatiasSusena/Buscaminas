84a5817ba662694637b7b5e95de9bfd5deee3121
Matías Susena Tizón <roexone@gmail.com>
2013-04-22 11:23:26 +0200

    Añadidos comentarios de lo ya programado
    Actualizado con comentarios de lo realizado hasta la fecha

--- Buscaminas/Buscaminas/Form1.cs
+++ Buscaminas/Buscaminas/Form1.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
  *  NOMBRE: Matías
  *  APELLIDOS: Susena Tizón
  *  1º D.A.M.
- *  ESTO ES UNA PRUEBA DE GITHUB
+ *  
  * 
  */
 
@@ -25,24 +25,37 @@ namespace Buscaminas
 
         public Form1()
         {
+            //inicializo 
             InitializeComponent();
-            int filas = 15;
-            int columnas = 20;
+            int columnas = 15;
+            int filas = 20;
             int anchoBoton = 20;
 
+            //calculo el ancho y el alto de la pantalla
+            //partiendo del alto y ancho de cada boton y
+            //multiplicandolo por el numero de botones que
+            //quiero crear
             this.Height = columnas * anchoBoton + 40;
             this.Width = filas * anchoBoton + 20;
 
+            //Declaro un array de 2 dimensiones(2D) de botones
             matrizBotones = new Button[filas, columnas];
 
+            //creo los botones
             for (int i = 0; i < filas; i++)
                 for (int j = 0; j < columnas; j++)
                 {
+                    //genera el nuevo boton
                     Button boton = new Button();
-                    boton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                    //boton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+
+                    //les asigno un ancho y un alto
                     boton.Width = anchoBoton;
                     boton.Height = anchoBoton;
+                    //le asigno/pongo una coordenada.
+                    //Nos devuelve la coordenada donde va el boton
                     boton.Location = new Point(i * anchoBoton, j * anchoBoton);
+                    //Esta es la forma de añadir / asociar un evento a mano
                     boton.Click += chequeaBoton;
                     matrizBotones[i, j] = boton;
                     panel1.Controls.Add(boton);
