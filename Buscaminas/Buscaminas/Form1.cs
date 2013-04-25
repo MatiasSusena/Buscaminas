499baceaa2887e4d41913e287fff898ab3d558e6
Matías Susena Tizón <roexone@gmail.com>
2013-04-25 13:39:12 +0200

    Con minas "B" y progresando
    Con minas y pinta todo de color fucsia

--- Buscaminas/Buscaminas/Form1.cs
+++ Buscaminas/Buscaminas/Form1.cs
@@ -22,14 +22,18 @@ namespace Buscaminas
     {
         //declaro el array de botones
         Button[,] matrizBotones;
+            int columnas = 20;
+            int filas = 20;
+            int anchoBoton = 20;
+            int minas = 20;
 
+        //si el tag es 1 es que no hay bomba
+        //si el tag es 2 es que si hay bomba
         public Form1()
         {
             //inicializo 
             InitializeComponent();
-            int columnas = 15;
-            int filas = 20;
-            int anchoBoton = 20;
+
 
             //calculo el ancho y el alto de la pantalla
             //partiendo del alto y ancho de cada boton y
@@ -57,14 +61,71 @@ namespace Buscaminas
                     boton.Location = new Point(i * anchoBoton, j * anchoBoton);
                     //Esta es la forma de añadir / asociar un evento a mano
                     boton.Click += chequeaBoton;
+                    boton.Tag = "1";
                     matrizBotones[i, j] = boton;
                     panel1.Controls.Add(boton);
                 }
+
+            poneMinas();
+        }
+
+
+        private void poneMinas() {
+            Random aleatorio = new Random();
+            int x, y = 0;
+            for (int i = 0; i < minas; i++) 
+            {
+                x = aleatorio.Next(filas);
+                y = aleatorio.Next(columnas);
+                while (!matrizBotones[y, x].Tag.Equals("1"))
+                {
+                    x = aleatorio.Next(filas);
+                    y = aleatorio.Next(columnas);
+                }
+                matrizBotones[y, x].Tag = "2";
+                matrizBotones[y, x].Text = "B";
+            }
         }
 
         private void chequeaBoton(object sender, EventArgs e)
-        {
-            (sender as Button).Enabled = false;
+        {   
+            //(sender as Button).Enabled = false;
+            Button b = (sender as Button);
+            //b.BackColor = Color.DarkKhaki;
+            int columna = b.Location.X / anchoBoton;
+            int fila = b.Location.Y / anchoBoton;
+
+
+
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if ((columna + j < columnas) && (columna + j>=0) && (fila +i <filas) && (fila +i >= 0))
+                    {
+                        if (matrizBotones[columna + j, fila + i].BackColor != Color.Fuchsia) {
+                    matrizBotones[columna+j, fila+i].BackColor = Color.Fuchsia;
+                    chequeaBoton(matrizBotones[columna + j,fila + i], e);
+                        }
+                    }
+                }
+            }
+
+
+            //    //Pintamos de fuchsia usando la matriz y no el sender
+            //    //Pintamos a la izquierda y derecha de nuestro boton pulsado
+            //    matrizBotones[columna - 1, fila].BackColor = Color.Fuchsia;
+            //matrizBotones[columna, fila].BackColor = Color.Fuchsia;
+            //matrizBotones[columna+1, fila].BackColor = Color.Fuchsia;
+
+            ////Pintamos los 3 botones de arriba de nuestro boton pulsado
+            //matrizBotones[columna - 1, fila-1].BackColor = Color.Fuchsia;
+            //matrizBotones[columna, fila-1].BackColor = Color.Fuchsia;
+            //matrizBotones[columna + 1, fila-1].BackColor = Color.Fuchsia;
+
+            ////Pintamos los 3 botones de abajo de nuestro boton pulsado
+            //matrizBotones[columna - 1, fila +1].BackColor = Color.Fuchsia;
+            //matrizBotones[columna, fila +1].BackColor = Color.Fuchsia;
+            //matrizBotones[columna + 1, fila +1].BackColor = Color.Fuchsia;
+
 
         }
     }
